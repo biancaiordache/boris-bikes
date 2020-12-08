2e9d2fac29c26067d7c2445de236e7ed491df46d
Bianca <biannca.iordache@gmail.com>
2020-12-08 16:30:33 +0000

    add a couple more unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+
+  it 'realeases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
